48c314f1f778f21eae3caf04d6cea9345d214440
Elbert Chan <elb.chan@gmail.com>
2022-03-08 13:24:26 -0800

    lunchtime version?

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -4,8 +4,21 @@ $.ajaxPrefilter(function (settings, _, jqXHR) {
   jqXHR.setRequestHeader('Authorization', API_KEY);
 });
 
-var searchYouTube = (query, callback) => {
-  // TODO
+console.log(YOUTUBE_API_KEY);
+
+var searchYouTube = (query, callback, errorCB = null) => {
+  $.ajax({
+    url: 'https://app-hrsei-api.herokuapp.com/api/recastly/videos',
+    type: 'GET',
+    data: 'cat',
+    //contentType: 'application/json',
+    dataType: 'json',
+    success: console.log(''),
+    error: console.log('') || function(error) {
+      console.error('chatterbox: Failed to fetch message', error);
+    }
+  });
 };
 
 export default searchYouTube;
+
